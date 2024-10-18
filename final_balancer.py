6760529195ce50207eee382f96e737f21bfbfcce
Jhewu <junyi.hewu2003@gmail.com>
2024-10-18 14:42:15 -0400

    finished the final balancer as a hardcode solution, but then found out, the pattern is bigger than I thought, therefore I really need to fix the data balancer

--- final_balancer.py
+++ final_balancer.py
@@ -1,11 +1,11 @@
-# Check if the dataset is balanced
+""" 
+Since the data balancer has a bug where the smaller
+site ID has 1 image less than the others, this is 
+a temporarily hard code solution to fix the issue
+"""
 
 import os
-import shutil
 import random
-import cv2 as cv
-from math import floor
-from math import ceil
 
 def SortDict(dict):
    # Sort the dictionary by its value in ascending order
@@ -25,6 +25,10 @@ def DataBalancer(folder):
     
     # declare dictionary to count site_id 
     site_ids = {}
+    
+    # declare dictionary to store the full_paths of
+    # each image within a site id
+    site_path = {} # dictionary with a list inside
 
     # count site_id and append full_file_path
     total_files = 0
@@ -36,30 +40,32 @@ def DataBalancer(folder):
     
         if site_id in site_ids: 
             site_ids[site_id] += 1
+            site_path[site_id].append(full_file_path)
+
         else:
             site_ids[site_id] = 1
+            site_path[site_id] = [full_file_path]
 
     # sort the dictionary, and turn it into a list
     a_list = SortDict(site_ids)
 
-    a_length = len(a_list)
+    # get the min
+    min = a_list[0][1]
+    print(f"\nHere's min {min} \n")
 
-    # count how many images in the list
-    for i in range(len(a_list)-1):
-        j = i+1
-        if a_list[i][1] != a_list[j][1]:
-            print("\nThe dataset is not balanced\n")
-            print("\nHere's the distribution: \n")
-            print(a_list)
-            return
+    # remove it from the list
+    a_list.remove(a_list[0])
 
-    print("\nThe dataset is balanced \n")
-    print("\nHere's the distribution: \n")
-    print(a_list)
-    print(f"\nThere should be: {a_length * a_list[0][1]} number of images\n")
+    # iterate over the list and remove 1 image per site ID
+    for item in a_list: 
+        if item[1] > min:
+            random.shuffle(site_path[item[0]])
+            os.remove(site_path[item[0]][0])
 
+    print(f"Finished balancing your data, check with is_data_balanced?.py")
 
 if __name__ == "__main__":
-    folder = "balanced_4"
+    folder = "balanced_3"
     DataBalancer(folder)
+
 
